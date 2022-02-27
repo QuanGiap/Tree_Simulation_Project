4cdfdb1d901180331f4a408f767eace339019702
QuanGiap <minhquangiapnguyen@gmail.com>
2022-02-27 14:21:47 -0800

    TreeGui Update

--- Tree_Simulation_Project/src/TreeGui.cpp
+++ Tree_Simulation_Project/src/TreeGui.cpp
@@ -1,6 +1,6 @@
 #include "TreeGui.h"
 
-TreeGui::TreeGui(int height, int width)
+TreeGui::TreeGui(int height, int width,TreeBaseBehavior& tree)
 {
-
+    behav = &tree;
 }
--- Tree_Simulation_Project/src/TreeGui.h
+++ Tree_Simulation_Project/src/TreeGui.h
@@ -1,13 +1,15 @@
 #ifndef TREEGUI_H
 #define TREEGUI_H
+#include "TreeBaseBehavior.h"
 #include "gwindow.h"
 #include "gbutton.h"
 using namespace sgl;
 class TreeGui
 {
 public:
-    TreeGui(int height, int width);
+    TreeGui(int height, int width, TreeBaseBehavior& behavior);
 private:
+    TreeBaseBehavior* behav;
     GWindow* gwin;
     GButton* tickButton;
 };
