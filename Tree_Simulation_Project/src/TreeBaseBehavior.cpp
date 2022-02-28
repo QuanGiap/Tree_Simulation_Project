a259e04010a3d32aa3a588d728ef33bf5e80bbc9
QuanGiap <minhquangiapnguyen@gmail.com>
2022-02-27 17:10:30 -0800

    Update

--- Tree_Simulation_Project/src/TreeBaseBehavior.cpp
+++ Tree_Simulation_Project/src/TreeBaseBehavior.cpp
@@ -10,33 +10,32 @@ bool TreeBaseBehavior::isOld() const{
 }
 
 void TreeBaseBehavior::update(){
-    grow(false);
+    grow(rand()%4==0);
 }
 
 void TreeBaseBehavior::growWidth(){
     int midPos = tree->getWidthData()/2;
-    int posSet = midPos-1-(tree->getWidth()/2);
-    //add wood on the left side tree
-    for(int i = 0;i < tree->getHeight() - tree->getWidth();i++){
-        tree->setTrue(i,posSet);
-    }
-    //add wood in the right side tree
-    posSet = midPos+1+(tree->getWidth()/2);
-    for(int i = 0;i < tree->getHeight()-tree->getWidth();i++){
-        tree->setTrue(i,posSet);
+    int offSet = 1+(tree->getWidth()/2);
+    int startSet = tree->getHeight() - tree->getWidth()+(tree->getWidth()/2);
+    for(int i = startSet;i >=0;i--){
+        //add wood on the left side tree
+        tree->setTrue(tree->getHeightData()-i-1,midPos-offSet);
+        //add wood in the right side tree
+        tree->setTrue(tree->getHeightData()-i-1,midPos+offSet);
     }
     tree->getWidth()+=2;
 }
 void TreeBaseBehavior::grow(bool isGrowW){
     if(isGrowW) growWidth();
     int setPosTime = tree->getWidth()/2+1;
-    if(isGrowW) setPosTime -= 2;
+    if(isGrowW) setPosTime -= 1;
     int midPos = tree->getWidthData()/2;
+    int startSet = tree->getHeightData() - tree->getHeight() - 1;
     for(int i = 0;i<setPosTime;i++){
         //add wood on the left side tree
-        tree->setTrue(tree->getHeight()+1-i,midPos-i);
+        tree->setTrue(startSet+i,midPos-i);
         //add wood on the right side tree
-        tree->setTrue(tree->getHeight()+1-i,midPos+i);
+        tree->setTrue(startSet+i,midPos+i);
     }
     tree->getHeight()+=1;
     tree->getAge()+=1;
