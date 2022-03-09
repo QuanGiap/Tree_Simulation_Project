91d17fd6c384e4a76547c32d4a1051e8d84a3b3e
NielsLarson <100330504+NielsLarson@users.noreply.github.com>
2022-03-09 15:40:02 -0800

    giveFruit update

--- Recreate folder/game-of-life/game-of-life/src/Adult.cpp
+++ Recreate folder/game-of-life/game-of-life/src/Adult.cpp
@@ -9,16 +9,24 @@ Adult::Adult(TreeBase& tree):TreeBaseBehavior(tree){
 void Adult::giveFruit(){
     srand(time(NULL));
     vector<int> position;
+    int treeArea = getTreeBase().getWidth() * getTreeBase().getHeight();
+    int areaMulti = treeArea % 10;
+    int waterMulti = 1;
     int possWidth = getTreeBase().getWidth() * 3;
     int a = possWidth;
     int b = getTreeBase().getHeight();
     int h = getTreeBase().getPlantPos();
     int k = getTreeBase().getHeight();
-    int x = h - (a / 2) + (rand() % possWidth);
-    int possHeight = sqrt((b * b) * ((a * a) - (h * h) + (2 * h * x) - (x * x) / (a * a))) * 2;
-    int y = k - (b / 2) + (rand() % possHeight);
-    getTreeBase().setFruitLocate().push_back(x);
-    getTreeBase().setFruitLocate().push_back(y);
+    if(getTreeBase().isWatered()) {
+        waterMulti = 2;
+    }
+    for(int i = 0; i < waterMulti * areaMulti; i++) {
+        int x = h - (a / 2) + (rand() % possWidth);
+        int possHeight = sqrt((b * b) * ((a * a) - (h * h) + (2 * h * x) - (x * x) / (a * a))) * 2;
+        int y = k - (b / 2) + (rand() % possHeight);
+        getTreeBase().setFruitLocate().push_back(x);
+        getTreeBase().setFruitLocate().push_back(y);
+    }
 }
 bool Adult::isOld()const{
     return getTreeBase().getAge()>650;
